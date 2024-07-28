1e0c3bb4df3907507e95fbf3fccffbfb9fe9c31c
Peter A. <ink.splatters@pm.me>
2024-07-28 10:49:33 +0200

    restored .pre-commit-config.yaml

--- flake.nix
+++ flake.nix
@@ -3,21 +3,9 @@
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
 
-    gitignore = {
-      url = "github:hercules-ci/gitignore.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     flake-utils = {
       url = "github:numtide/flake-utils";
       inputs.systems.follows = "systems";
-    };
-    git-hooks = {
-      url = "github:cachix/git-hooks.nix";
-      inputs = {
-        gitignore.follows = "gitignore";
-        nixpkgs.follows = "nixpkgs";
-        nixpkgs-stable.follows = "nixpkgs";
-      };
     };
   };
 
@@ -30,12 +18,11 @@
     ];
   };
 
-  outputs = { self, flake-utils, git-hooks, nixpkgs,...  }:
+  outputs = { self, flake-utils,  nixpkgs,...  }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let 
           pkgs = nixpkgs.legacyPackages.${system};
-          pre-commit-check = import ./nix/pre-commit-check.nix { inherit pkgs git-hooks system; };
         in
         with pkgs;
         {
@@ -45,19 +32,6 @@
 
           devShells = {
             default = callPackage ./nix/shell.nix {};
-
-            install-hooks = mkShell.override { stdenv = stdenvNoCC; } {
-              inherit system;
-              shellHook =
-              let
-                inherit (pre-commit-check) shellHook;
-              in
-              ''
-                ${shellHook}
-                echo Done!
-                exit
-              '';
-            };
         };
 
         formatter = nixfmt-rfc-style;
